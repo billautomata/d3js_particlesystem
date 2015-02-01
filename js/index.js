6ae45aeaf9b0ae3ceac69c6a516353c91b8660bf
billautomata <bill@evident.io>
2015-02-01 14:04:00 -0800

    yep

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+'use strict';
 //
 // particle system with toxiclibs.js
 //
@@ -6,11 +7,12 @@
 // * * *
 
 
-
 var w = window.innerWidth
 var h = window.innerHeight
 
-var n_elements = 128
+var random = d3.random.normal(0)
+
+var n_elements = 256
 
 var svg = d3.select('body').append('svg')
 
@@ -18,16 +20,22 @@ svg.attr('width', w)
    .attr('height', h)
    .style('background-color', 'black')
 
+var g_root = svg.append('g')
+  .attr('transform', 'translate(' + (w*0.5) + ',' + (h*0.5) + ')')
+
 var simplexNoise = toxi.math.noise.simplexNoise.noise
 
 var particles = []
 for (var i = 0; i < n_elements; i++){
+
   particles.push({
-    x: Math.random() * w,
-    y: Math.random() * h,
-    vx: Math.random()-0.5,
-    vy: Math.random()-0.5
+    pos: new toxi.geom.Vec2D(random()*w, random()*h),
+    //vel: new toxi.geom.Vec2D(random(),random()),
+    vel: new toxi.geom.Vec2D(0,0),
+    r: (Math.random() * (w*0.02)) + 1,
+    is_transitioned: false
   })
+
 }
 
 var circles = []
@@ -36,10 +44,14 @@ for(var i = 0; i < n_elements; i++){
 
   var particle = particles[i]
 
-  var circle = svg.append('circle').attr('cx', particle.x).attr('cy', particle.y).attr('r', Math.random()*3+1)
-  circle.style('fill-opacity', 0.8)
-  circle.style('fill', 'cyan')
-  circle.style('stroke', 'none')
+  var circle = g_root.append('circle')
+    .attr('cx', particle.x)
+    .attr('cy', particle.y)
+    .attr('r', particle.r)
+
+  circle.style('fill-opacity', 0.5)
+    .style('fill', d3.rgb(Math.random()*255, Math.random()*255, Math.random()*255))
+    .style('stroke', 'none')
 
   // data binding like a pro
   // duck-typing
@@ -56,77 +68,81 @@ var mouse = {
   y: h*0.5
 }
 
+var voro = d3.geom.voronoi().x(function(d){ return d.x }).y(function(d){ return d.y })
+
+var m  = voro(circles)
+
+var center = new toxi.geom.Vec2D(0,0)
+var force_vec = new toxi.geom.Vec2D(0,0)
+
+
+
+var particle
+var noise_multi = 0.3
+var time_multi = 0.0003
+var time = Date.now() * time_multi
+var dampening
+var svg_element
+var distance_to_center
+var i
 
 tick()
 
 function tick(){
-  circles.forEach(function(svg_element,index){
 
-    var particle = svg_element.particle
+  //console.profile()
+  time = Date.now() * time_multi
 
+  for(i = -1; ++i < circles.length;){
 
+    svg_element = circles[i]
 
-    var noise_multi = 0.1
+    particle = svg_element.particle
 
-    var time = Date.now() * 0.0001
+    particle.vel.x += noise_multi * simplexNoise(particle.pos.x/w,particle.pos.y/h,time)
+    particle.vel.y += noise_multi * simplexNoise((h-particle.pos.y)/h,(w-particle.pos.x)/w,time)
 
-    particle.vx += noise_multi * simplexNoise(particle.x/w,particle.y/h,time)
-    particle.vy += noise_multi * simplexNoise((h-particle.y)/h,(w-particle.x)/w,time)
+    dampening = 0.991 - (particle.r * particle.r * 0.00001)
 
-    var dampening = 0.991
-    particle.vx *= dampening
-    particle.vy *= dampening
+    particle.vel.x *= dampening
+    particle.vel.y *= dampening
 
-    particle.x += particle.vx
-    particle.y += particle.vy
+    particle.pos.x += particle.vel.x
+    particle.pos.y += particle.vel.y
 
-    if(particle.x > w){
-      particle.x -= w
+    if(particle.pos.x > w){
+      particle.pos.x -= w * 2
     }
-    if(particle.x < 0){
-      particle.x += w
+    if(particle.pos.x < -w){
+      particle.pos.x += w * 2
     }
-    if(particle.y > h){
-      particle.y -= h
+    if(particle.pos.y > h){
+      particle.pos.y -= h * 2
     }
-    if(particle.y < 0){
-      particle.y += h
-    }
-
-    svg_element.attr('cx', particle.x)
-    svg_element.attr('cy', particle.y)
-
-    if(false){
-      var distance = Math.sqrt(Math.pow(particle.x-mouse.x,2)+Math.pow(particle.y-mouse.y,2))
-
-      if(distance<100){
-        if(!svg_element.in_range){
-//          svg_element.transition().attr('r', Math.random()*50+10).ease('bounce').duration(1000)
-          svg_element.transition()
-            .duration(500)
-            .ease('bounce')
-            .style('fill', d3.rgb(Math.random()*255, Math.random()*255, Math.random()*255))
-            .attr('r', 50)
-        }
-        svg_element.in_range = true
-      } else {
-        if(svg_element.in_range){
-
-          svg_element.transition()
-            .ease('bounce')
-            .delay(500)
-            .duration(1000)
-            .attr('r', Math.random()*3+1)
-
-        }
-        svg_element.in_range = false
-      }
+    if(particle.pos.y < -h){
+      particle.pos.y += h * 2
     }
 
+    svg_element.attr('cx', particle.pos.x).attr('cy', particle.pos.y)
+    //svg_element.attr('cy', particle.pos.y)
 
-  })
+    distance_to_center = Math.sqrt(particle.pos.x*particle.pos.x + particle.pos.y * particle.pos.y )
+    force_vec.x = particle.pos.x
+    force_vec.y = particle.pos.y
 
-  window.requestAnimationFrame(tick)
+    force_vec.normalizeTo(distance_to_center*0.0001)
+
+
+    particle.vel.x -= force_vec.x
+    particle.vel.y -= force_vec.y
+
+
+  }
+
+  //console.profileEnd()
+  // debugger;
+  //
+  return window.requestAnimationFrame(tick)
 
 }
 
